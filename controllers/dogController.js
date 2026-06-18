4dededcd4ace332f41d464871271ba1084602065
Himakshi <chhimakshi@gmail.com>
2026-06-18 15:41:05 +0530

    fixed post issue

--- controllers/dogController.js
+++ controllers/dogController.js
@@ -1,42 +1,137 @@
 const Dog = require("../models/Dog");
 const cloudinary = require("../config/cloudinary");
+const fs = require("fs");
+
+// ADD DOG
 const addDog = async (req, res) => {
   try {
-
-    console.log("BODY:", req.body);
-    console.log("FILE:", req.file);
-
     if (!req.file) {
       return res.status(400).json({
-        message: "Image file not received"
+        message: "Please upload an image",
       });
     }
 
     const result = await cloudinary.uploader.upload(
       req.file.path,
       {
-        folder: "dogs"
+        folder: "dogs",
       }
     );
+
+    // delete local file after upload
+    fs.unlinkSync(req.file.path);
 
     const dog = await Dog.create({
       breed: req.body.breed,
       age: req.body.age,
       color: req.body.color,
-      image: result.secure_url
+      image: result.secure_url,
     });
 
     res.status(201).json({
       message: "Dog Added Successfully",
-      dog
+      dog,
     });
-
   } catch (err) {
     console.log(err);
 
     res.status(500).json({
-      message: err.message
+      message: err.message,
     });
   }
 };
-module.exports = { addDog, getDogs, getDogById, updateDog, deleteDog };
+
+// GET ALL DOGS
+const getDogs = async (req, res) => {
+  try {
+    const dogs = await Dog.find();
+
+    res.status(200).json({
+      count: dogs.length,
+      dogs,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+// GET DOG BY ID
+const getDogById = async (req, res) => {
+  try {
+    const dog = await Dog.findById(req.params.id);
+
+    if (!dog) {
+      return res.status(404).json({
+        message: "Dog Not Found",
+      });
+    }
+
+    res.status(200).json(dog);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+// UPDATE DOG
+const updateDog = async (req, res) => {
+  try {
+    const dog = await Dog.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!dog) {
+      return res.status(404).json({
+        message: "Dog Not Found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Dog Updated Successfully",
+      dog,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+// DELETE DOG
+const deleteDog = async (req, res) => {
+  try {
+    const dog = await Dog.findByIdAndDelete(
+      req.params.id
+    );
+
+    if (!dog) {
+      return res.status(404).json({
+        message: "Dog Not Found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Dog Deleted Successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+module.exports = {
+  addDog,
+  getDogs,
+  getDogById,
+  updateDog,
+  deleteDog,
+};
